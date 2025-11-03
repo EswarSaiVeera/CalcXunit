51394629c68f7a035c47cad4caf640ad7a94cbcc
EswarSaiVeera <eswarsaiveera@gmail.com>
2025-11-03 10:51:27 +0530

    Add project files.

---
+++ CalcXunit/CalcXtesting.cs
@@ -0,0 +1,42 @@
+﻿using CalcXunit;
+using Xunit;
+
+public class CalcXtesting
+{
+    [Fact]
+    public void Add_Returns_Sum()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 3));
+    }
+
+    [Fact]
+    public void Subtract_Returns_Difference()
+    {
+        var calc = new Calculator();
+        Assert.Equal(1, calc.Subtract(4, 3));
+    }
+
+    [Fact]
+    public void Multiply_Returns_Product()
+    {
+        var calc = new Calculator();
+        Assert.Equal(6, calc.Multiply(2, 3));
+    }
+
+    [Theory]
+    [InlineData(6, 2, 3)]
+    [InlineData(9, 3, 3)]
+    public void Divide_Returns_Quotient(int dividend, int divisor, double expected)
+    {
+        var calc = new Calculator();
+        Assert.Equal(expected, calc.Divide(dividend, divisor));
+    }
+
+    [Fact]
+    public void Divide_ByZero_ThrowsException()
+    {
+        var calc = new Calculator();
+        Assert.Throws<ArgumentException>(() => calc.Divide(1, 0));
+    }
+}
---
+++ CalcXunit/Calculator.cs
@@ -0,0 +1,15 @@
+﻿namespace CalcXunit
+{
+    public class Calculator
+    {
+        public int Add(int a, int b) => a + b;
+        public int Subtract(int a, int b) => a - b;
+        public int Multiply(int a, int b) => a * b;
+        public double Divide(int a, int b)
+        {
+            if (b == 0) throw new ArgumentException("Cannot divide by zero.");
+            return (double)a / b;
+        }
+
+    }
+}
